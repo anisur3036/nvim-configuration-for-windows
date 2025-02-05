9dbf3c8df96b637f205f50d0c31abb4eb5fa843b
Anisur Rahman <anisurlist3@gmail.com>
2025-02-05 09:14:31 +0600

    dressing plugin added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,29 @@
 --     }
 --   end,
 -- }
-
+--
 return {
-  "olimorris/onedarkpro.nvim", name="onedarkpro",
-  priority = 1000, -- Ensure it loads first
-  config = function()
-    require("onedarkpro").setup {
-      vim.cmd("colorscheme onedark")
-    }
-  end,
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+        transparent = true,
+        italic_comments = true,
+        borderless_telescope = false,
+      }
+      vim.cmd [[colorscheme cyberdream]]
+    end,
+  },
 }
+
+-- return {
+--   "olimorris/onedarkpro.nvim", name="onedarkpro",
+--   priority = 1000, -- Ensure it loads first
+--   config = function()
+--     require("onedarkpro").setup {
+--       vim.cmd("colorscheme onedark")
+--     }
+--   end,
+-- }
