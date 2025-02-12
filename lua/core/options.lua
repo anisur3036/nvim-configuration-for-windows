ce25227504e55cbd6203cf15015182050764d76a
Anisur Rahman <anisurlist3@gmail.com>
2025-02-12 20:28:23 +0600

    system clipboard copy paste added

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 vim.opt.encoding = "utf-8" -- set encoding
 
@@ -23,6 +23,8 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.opt.clipboard = "unnamedplus"
+
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -35,23 +37,23 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.autoindent = true -- auto indentation
-vim.opt.list = true -- show tab characters and trailing whitespace
+vim.opt.expandtab = true     -- convert tabs to spaces
+vim.opt.autoindent = true    -- auto indentation
+vim.opt.list = true          -- show tab characters and trailing whitespace
 
-vim.opt.ignorecase = true -- ignore case when searching
-vim.opt.smartcase = true -- unless capital letter in search
+vim.opt.ignorecase = true    -- ignore case when searching
+vim.opt.smartcase = true     -- unless capital letter in search
 
-vim.opt.hlsearch = false -- do not highlight all matches on previous search pattern
-vim.opt.incsearch = true -- incrementally highlight searches as you type
+vim.opt.hlsearch = false     -- do not highlight all matches on previous search pattern
+vim.opt.incsearch = true     -- incrementally highlight searches as you type
 
 vim.opt.termguicolors = true -- enable true color support
 
-vim.opt.scrolloff = 8 -- minimum number of lines to keep above and below the cursor
-vim.opt.sidescrolloff = 8 --minimum number of columns to keep above and below the cursor
+vim.opt.scrolloff = 8        -- minimum number of lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8    --minimum number of columns to keep above and below the cursor
 
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.py",
   callback = function()
     vim.opt.textwidth = 79
@@ -59,8 +61,8 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
   end
 }) -- python formatting
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {"*.js", "*.html", "*.css", "*.lua"},
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.js", "*.html", "*.css", "*.lua" },
   callback = function()
     vim.opt.tabstop = 2
     vim.opt.softtabstop = 2
@@ -69,13 +71,10 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
 }) -- javascript formatting
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = function()
-      if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-        vim.cmd("normal! g`\"")
-      end
+  pattern = "*",
+  callback = function()
+    if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.cmd("normal! g`\"")
     end
+  end
 }) -- return to last edit position when opening files
-
-
-
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,11 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'cyberdream',
-      },
-    })
+    -- require('lualine').setup({
+    --   options = {
+    --     theme = 'cyberdream',
+    --   },
+    -- })
   end
 
 }
